035596097db698468b2141d8bc851a84f9e8a40f
ZedPea <zp+github@zacharypalmer.co.uk>
2017-10-09 16:40:58 +0100

    Add missed file

--- client/src/exe/Main.hs
+++ client/src/exe/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveGeneric #-}
-
 module Main
 (
     main
@@ -7,48 +5,104 @@ module Main
 where
 
 import Data.Binary (Binary, encode, decodeOrFail)
+import Data.Binary.Get (ByteOffset)
 import qualified Data.ByteString as BS (ByteString)
 import qualified Data.ByteString.Lazy as BL (ByteString)
-import Data.Binary.Get (ByteOffset)
+
 import Network.Socket.ByteString (send, recv)
 import Data.ByteString.Lazy (toStrict, fromStrict)
-import GHC.Generics (Generic)
 import System.IO (hFlush, stdout)
-import Control.Monad (forever)
-import Data.UUID.Types (UUID)
-
-import Types (Message(..), ActionMessage(..))
+import Control.Monad.Trans.State (evalStateT)
+import Control.Monad.Trans.Class (lift)
+import Control.Lens ((^.))
 
 import Network.Socket 
     (Socket, getAddrInfo, socket, addrFamily, addrProtocol, addrSocketType, 
      addrAddress, connect)
 
+import Lenses (cgame)
+
+import Types 
+    (Message(..), ActionMsg, PlayerTurnMsg, CardMsg, DealtCardsMsg, 
+     PotWinnersMsg, GameOverMsg, PlayersRemovedMsg, CardRevealMsg, 
+     CGameStateT, ClientGame)
+
 main :: IO ()
 main = do   
+    (initialState, sock) <- initialSetup
+    
+    evalStateT (ioLoop sock) initialState 
+
+initialSetup :: IO (ClientGame, Socket)
+initialSetup = do
     addr:_ <- getAddrInfo Nothing (Just "127.0.0.1") (Just "2112")
     sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
 
     connect sock (addrAddress addr)
 
+    name <- getName
+    
+    send sock (toStrict . encode $ name)
+
+    msg <- recv sock 4096
+
+    case decode msg of
+        Left (_, _, err) -> error err
+        Right (_, _, msg') -> case msg' of
+            MIsInitialGame m -> return (m^.cgame, sock)
+            _ -> error "Invalid message recieved!"
+
+getName :: IO String
+getName = do
     putStr "Enter your name: "
     hFlush stdout
-    input <- getLine
+    getLine
 
-    send sock (toStrict . encode $ input)
-    
-    forever (ioLoop sock)
-
-ioLoop :: Socket -> IO ()
+ioLoop :: Socket -> CGameStateT ()
 ioLoop sock = do
-    msg <- decode <$> recv sock 4096
+    msg <- lift $ decode <$> recv sock 4096
     case msg of
-        Left (_, _, err) -> putStrLn err
-        Right (_, _, msg') -> handleMsg msg'
+        Left (_, _, err) -> error err
+        Right (_, _, msg') -> do
+            handleMsg msg'
+            ioLoop sock
 
 decode :: (Binary a) => BS.ByteString -> 
                         Either (BL.ByteString, ByteOffset, String)
                                (BL.ByteString, ByteOffset, a)
 decode msg = decodeOrFail $ fromStrict msg
 
-handleMsg :: Message -> IO () -- how do we make this typecheck?
-handleMsg = undefined
+handleMsg :: Message -> CGameStateT ()
+handleMsg msg = case msg of
+    MIsAction m -> handleAction m
+    MIsPlayerTurn m -> handlePlayerTurn m
+    MIsCard m -> handleNewCards m
+    MIsDealt m -> handleMyCards m
+    MIsPotWinners m -> handlePotWinners m
+    MIsGameOver m -> handleGameOver m
+    MIsPlayersRemoved m -> handlePlayersRemoved m
+    MIsCardReveal m -> handleCardsRevealed m
+
+handleAction :: ActionMsg a -> CGameStateT ()
+handleAction = undefined
+
+handlePlayerTurn :: PlayerTurnMsg -> CGameStateT ()
+handlePlayerTurn = undefined
+
+handleNewCards :: CardMsg -> CGameStateT ()
+handleNewCards = undefined
+
+handleMyCards :: DealtCardsMsg -> CGameStateT ()
+handleMyCards = undefined
+
+handlePotWinners :: PotWinnersMsg -> CGameStateT ()
+handlePotWinners = undefined
+
+handleGameOver :: GameOverMsg -> CGameStateT ()
+handleGameOver = undefined
+
+handlePlayersRemoved :: PlayersRemovedMsg -> CGameStateT ()
+handlePlayersRemoved = undefined
+
+handleCardsRevealed :: CardRevealMsg -> CGameStateT ()
+handleCardsRevealed = undefined
