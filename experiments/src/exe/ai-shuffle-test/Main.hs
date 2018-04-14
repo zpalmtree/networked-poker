fbe08ea7d5b9316030541186ded296bc8d8a94de
ZedPea <zp+github@zacharypalmer.co.uk>
2018-04-14 22:34:11 +0100

    Compare effectiveness of shuffles against winrate
    Currently not working

--- experiments/src/exe/ai-shuffle-test/Main.hs
+++ experiments/src/exe/ai-shuffle-test/Main.hs
@@ -5,14 +5,24 @@ module Main
 where
 
 import System.IO (hFlush, stdout)
+import Control.Concurrent (forkIO, killThread, threadDelay)
+import Control.Concurrent.Async (concurrently)
+import Text.Printf (printf)
 
-
+import qualified AICallAny as CA (handleFunc)
+import qualified AIRuleBased as RB (handleFunc)
 import Utilities.Card (fullDeck)
 import LaunchServer (launchServerWithShuffle)
+import AIFramework (runAI)
 
 import Types
     (DrawAlgorithm(..), Deck(..), ShuffleType, RandomIndexDeck(..), 
      KnuthDeck(..), RandomSortDeck(..), ShuffleType(..))
+
+data WinRate = WinRate {
+    callAny :: Int,
+    ruleBased :: Int
+}
 
 main :: IO ()
 main = do
@@ -21,7 +31,37 @@ main = do
     algo <- getAlgo rng
     let deck = mkDeck rng
 
-    launchServerWithShuffle algo deck
+    serverThread <- forkIO $ launchServerWithShuffle algo deck
+
+    putStrLn "Launching server..."
+
+    -- wait for server to launch
+    threadDelay 5000000
+
+    putStrLn "Server started!"
+
+    winRates <- playLoop 1 (WinRate 0 0)
+
+    killThread serverThread
+
+    printf "Rule based AI won %d out of %d encounters\n" (ruleBased winRates)
+                                                         totalRuns
+
+totalRuns = 1000
+
+playLoop :: Int -> WinRate -> IO WinRate
+playLoop n winrate
+    | n > totalRuns = return winrate
+    | otherwise = do
+        printf "Executing game %d of %d\n" n totalRuns
+
+        (callAnyResult, ruleBasedResult) <- concurrently 
+                                            (runAI "call-any" CA.handleFunc)
+                                            (runAI "rule-based" RB.handleFunc)
+
+        playLoop (n+1) (if callAnyResult
+                            then winrate { callAny = callAny winrate + 1 }
+                            else winrate { ruleBased = ruleBased winrate + 1 })
 
 mkDeck :: DrawAlgorithm -> Deck
 mkDeck d = case d of
